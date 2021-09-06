411b02a6765e54d4680c49f45e34da4ec4f85768
Kevin Padilla <kevin0696s@gmail.com>
2021-09-06 19:00:21 -0400

    traying to create a spider in scrapy, im going to use bs4 better

---
+++ Modelo_Analisis_Sentimientos/Modelo_Analisis_Sentimientos/spiders/news-spider.py
@@ -0,0 +1,36 @@
+import scrapy 
+# from common import config
+import datetime
+class NewsSpider(scrapy.Spider):
+    name = "news"
+    start_urls = [
+        'https://lapatria.bo/'
+    ]
+    custom_settings = {
+        'FEED_URI': f'{datetime.date.today()}.csv',
+        'FEED_FORMAT': 'csv',
+        'ROBOTSTXT_OBEY': True,
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'USER_AGENT': 'Padi19'
+    }
+
+    
+
+    def parse_articles(self, response):
+        title = response.xpath('//div[@class="td-post-header"]//h1[@class="entry-title"]/text()').get()
+        article = response.xpath('//div[@class="td-post-content tagdiv-type"]/p/text()').getall()
+        yield {
+            'title': title,
+            'article': article
+        }
+    # This parse give urls of each new in the page
+    def parse(self, response):
+        todays_date = datetime.date.today()
+        
+        urls = response.xpath('//div[@class="td-block-span6"]//h3/a/@href').getall()
+        news_dates = response.xpath('//div[@class="td-block-span6"]//span[@class="td-post-date"]/time/@datetime').getall()
+        news_dates = list(map(lambda x: x.split("T")[0], news_dates))
+
+        for url,news_date in urls,news_dates:
+            if news_date == todays_date:
+                yield response.follow(url, callback=self.parse_articles)
---
+++ Modelo_Analisis_Sentimientos/Modelo_Analisis_Sentimientos/spiders/pruebas.py
@@ -0,0 +1,4 @@
+import datetime
+
+x = "its gonnaTbe legend.....dary"
+print(x.split("T")[0])
